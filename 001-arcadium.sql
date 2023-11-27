8fd90d9fddc7974d5bb4cba1c87f987508dbf72f
raquel-ogs <raquel.silva@sptech.school>
2023-11-27 19:37:14 -0300

    Remoção de tabela de sistemaOperacional

--- 001-arcadium.sql
+++ 001-arcadium.sql
@@ -4,16 +4,8 @@ CREATE DATABASE Arcadium;
 
 USE Arcadium;
 
-CREATE TABLE sistemaOperacional (
-id INT PRIMARY KEY AUTO_INCREMENT,
-distribuicao VARCHAR(20) NOT NULL,
-versionamento VARCHAR(20) NOT NULL
-);
-
 CREATE TABLE totem(
 id INT PRIMARY KEY,
-fksistemaoperacional INT, 
-CONSTRAINT fkso FOREIGN KEY (fksistemaoperacional) REFERENCES sistemaOperacional(id),
 dtInstalacao DATE,
 RAMtotal INT,
 CPUtotal DOUBLE,
